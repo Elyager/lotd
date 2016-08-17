1aad50d6377a8787376b634809595a912fee1ed8
Elyager <elyager@gmail.com>
2016-08-17 13:06:01 -0700

    Today News Updated! 👊

--- scripts/createTodayFile.sh
+++ scripts/createTodayFile.sh
@@ -3,5 +3,5 @@ _now=$(date +"%Y%m%d")
 _file="$HOME/Documents/Hublab/lotd/lotd_$_now.md"
 pwd
 echo "Creating file $_file to save links of the day ... in "
-echo "LOTD_$_now" > $_file
+echo "## LOTD_$_now" > $_file
 echo "File was created"
