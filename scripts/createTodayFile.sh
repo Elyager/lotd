a1ea316fcd39aa2f255819614ae57d0b752de8a3
Elyager <elyager@gmail.com>
2016-08-18 10:46:24 -0700

    Fix createTodayFile script path

--- scripts/createTodayFile.sh
+++ scripts/createTodayFile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _now=$(date +"%Y%m%d")
-_file="$HOME/Documents/Hublab/lotd/lotd_$_now.md"
+_file="$HOME/Documents/Hublab/lotd/content/lotd_$_now.md"
 pwd
 echo "Creating file $_file to save links of the day ... in "
 echo "## LOTD_$_now" > $_file
