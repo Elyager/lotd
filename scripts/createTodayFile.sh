43fbc812ca3b3814156dd56fb80d755af3b84bd4
Elyager <elyager@gmail.com>
2016-09-23 16:02:00 -0700

    Today News Updated! 👊

--- scripts/createTodayFile.sh
+++ scripts/createTodayFile.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 _now=$(date +"%Y%m%d")
-_file="$HOME/Documents/Hublab/lotd/content/lotd_$_now.md"
+_postName="LOTD-$_now"
+_file="$_postName.md"
+cd $HOME/Documents/Hublab/lotd/content/
 pwd
-echo "Creating file $_file to save links of the day ... in "
-echo "## LOTD_$_now" > $_file
+echo "Creating file $_file to save links of the day"
+echo "## LOTD-$_now" > $_file
 echo "File was created"
+cd "$HOME/Documents/HubLab/blogdelyager/source/_posts"
+pwd
+echo "Creating post in blogdelyager"
+hexo new post $_postName
+echo "Post created!"
+echo "Add lotd tag"
+pwd
+sed -e "4s/.*/tags: lotd/" $_file > "temp.md"
+mv temp.md $_file
--- scripts/pushToGitHub.sh
+++ scripts/pushToGitHub.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+_now=$(date +"%Y%m%d")
+_postName="LOTD-$_now"
+_file="$_postName.md"
 cd $HOME/Documents/HubLab/lotd
 git add .
 git commit -m "Today News Updated! 👊"
 git push
+echo "Toda News updated in lotd repo"
+echo "Publish post in blogdelyager"
+cd $HOME/Documents/HubLab/blogdelyager/source/_posts
+head -n 5 $_file > headers_temp.md
+tail -n +2 ../../../lotd/content/$_file > content_temp.md
+cat headers_temp.md content_temp.md > $_file
+_time=$(date +%H:%M:%S)
+echo >> $_file
+echo "Última actualización: **$_time**" >> $_file
+rm headers_temp.md content_temp.md
