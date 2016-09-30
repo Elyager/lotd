9defc211c0a4dc2d3efa9d02d857d2fcd3ed6e38
Elyager <elyager@gmail.com>
2016-09-30 15:48:48 -0700

    Today News Updated! 👊

--- scripts/pushToGitHub.sh
+++ scripts/pushToGitHub.sh
@@ -15,7 +15,7 @@ head -n 5 $_file > headers_temp.md
 tail -n +2 $HOME/Documents/HubLab/lotd/content/$_file > content_temp.md
 cat headers_temp.md content_temp.md > $_file
 _time=$(date +%H:%M:%S)
-echo >> $_file
+echo " \n" >> $_file
 echo "Última actualización: **$_time**" >> $_file
 rm headers_temp.md content_temp.md
 git add .
